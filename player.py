7824c34000b0feb63f4038091d632183bbc66271
deng-hua <dora_muu@outlook.com>
2021-12-02 18:13:07 -0800

    add pre-commit

--- player.py
+++ player.py
@@ -1,24 +1,26 @@
 class Player:
-    
-    
- 
     def __init__(self, name):
         self.name = name
         self.points = 99
         self.used_points_history = []
-        self.points_indicator_mapping = {range(1,20): '1~19',
-                                        range(20,40): '20~39',
-                                        range(40,60): '40~59',
-                                        range(60,80): '60~79',
-                                        range(80,100): '80~99'}
-        self.used_points_indicator_mapping = {range(1,10): 'Black', range(10,100): 'White' }
-   
+        self.points_indicator_mapping = {
+            range(1, 20): "1~19",
+            range(20, 40): "20~39",
+            range(40, 60): "40~59",
+            range(60, 80): "60~79",
+            range(80, 100): "80~99",
+        }
+        self.used_points_indicator_mapping = {
+            range(1, 10): "Black",
+            range(10, 100): "White",
+        }
+
     def displayLeftPointsIndicator(self):
         for i in self.points_indicator_mapping:
             if self.points in i:
                 indicator = self.points_indicator_mapping[i]
         print(f"{self.name}'s left points fall within {indicator}")
- 
+
     def usePoints(self, used_points):
         self.points = self.points - used_points
         self.used_points_history.append(used_points)
